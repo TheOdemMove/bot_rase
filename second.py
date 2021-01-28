8ed1a57b798ab5f6f98fffa8bebe53637fc50384
TheOdemMove <playcastro228@gmail.com>
2021-01-28 18:36:07 +0200

    28.01.2021 > добавил мп

--- second.py
+++ second.py
@@ -1,12 +1,17 @@
 import telebot
-from telebot import types #Подключили дополнения
+from telebot import types
 import datetime
 import sqlite3
+import telebot_calendar
+from telebot_calendar import CallbackData
+from telebot.types import ReplyKeyboardRemove, CallbackQuery
+
+
 
 bot = telebot.TeleBot('1452247200:AAES4nlaN4zg6-J6ljkDssrcfJYpBVdw3sU')
 now = datetime.datetime.now()
-
-
+calendar_1 = CallbackData("calendar_1", "action", "year", "month", "day")
+ccalendar = False
 
 
 @bot.message_handler(commands=['start'])
@@ -23,6 +28,8 @@ def phone(message):
         bot.send_message(message.from_user.id, "Используйте меню для выбора функций.", reply_markup=default_menu_user())
     elif check == 4:
         bot.send_message(message.from_user.id, "[You Admin] Используйте меню для выбора функций. ", reply_markup=default_menu_admin())
+
+
 
 
 def check_reg(id):
@@ -66,7 +73,7 @@ def work(message):
             ###
             bot.send_message(message.from_user.id, "Для того, что бы зарегистрироваться, следуйте дальнейшим инструкциям бота.", reply_markup=register)
             bot.send_message(message.from_user.id, "Введите ваше имя: ")
-            bot.register_next_step_handler(message, get_name);
+            bot.register_next_step_handler(message, get_name)
         else:
             bot.send_message(message.from_user.id, 'Простите, я не понимаю вас, используйте команду - /start', reply_markup=default_start_button())
     elif check == 2:
@@ -79,15 +86,100 @@ def work(message):
     elif check == 4:
         if message.text == 'Панель администратора':
             bot.send_message(message.from_user.id, "Используйте меню, для выбора действий.", reply_markup=default_menu_admin_action())
-            bot.register_next_step_handler(message, test);
+            bot.register_next_step_handler(message, admin_panel)
         elif message.text == 'Мои автомобили':
             get_car_user(message)
         else:
             bot.send_message(message.from_user.id, 'Простите, я не понимаю вас, используйте команду - /start', reply_markup=default_start_button())
 
-def test(message):
-    if message.text == "Выйди отсюда розбийнык":
-        bot.send_message(message.from_user.id, "Вы вернулись в панель администратора.", reply_markup=default_menu_admin())
+def admin_panel(message):
+    if message.text == 'Управление мероприятиями':
+        bot.send_message(message.from_user.id, "Вы перешли в меню управления мероприятиями.", reply_markup=default_mp_action())
+        bot.register_next_step_handler(message, menu_mp)
+    elif message.text == 'Cписок запросов на регистрацию':
+        testov(message)
+    elif message.text == '<< Назад':
+        bot.send_message(message.from_user.id, "Вы вернулись в главное меню.", reply_markup=default_menu_admin())
+
+def menu_mp(message):
+    if message.text == '<< Назад':
+        bot.send_message(message.from_user.id, "Вы вернулись в панель управления администратора.", reply_markup=default_menu_admin_action())
+        bot.register_next_step_handler(message, admin_panel)
+    elif message.text == 'Список мероприятий':
+        with sqlite3.connect("static/database/main.sqlite") as conn:
+            cursor = conn.cursor()
+            cursor.execute("CREATE TABLE IF NOT EXISTS MP (Id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, MpName TEXT NOT NULL, MpDate TEXT NOT NULL, MpTime TEXT NOT NULL, MpWeather TEXT NOT NULL, MpTemp INTEGER NOT NULL, MpMember INTEGER NOT NULL)")
+            cursor.execute("SELECT * FROM MP")
+            result = cursor.fetchall()
+            conn.commit()
+        if len(result) > 0:
+            mpbut = telebot.types.ReplyKeyboardMarkup(True, True)
+            for num in result:
+                mpbut.row('ID: {} | Name: {}\nDate: {} | Time: {}'.format(num[0], num[1], num[2], num[3]))
+            mpbut.row('<< Назад')
+            bot.send_message(message.from_user.id, "Здесь отображены крайние 10 мероприятий, для добавления результатов гонок выберите мероприятие.", reply_markup=mpbut)
+            bot.register_next_step_handler(message, view_mp)
+        else:
+            bot.send_message(message.from_user.id, "Похоже что никаких мероприятий не проводиться.", reply_markup=default_mp_action())
+            bot.register_next_step_handler(message, menu_mp)
+    elif message.text == 'Добавить мероприятие':
+        delbut = types.ReplyKeyboardRemove()
+        bot.send_message(message.from_user.id, "Для того что бы добавить мероприятие, следуйте инструкциям.", reply_markup=delbut)
+        bot.send_message(message.from_user.id, "Введите название мероприятия: ")
+        bot.register_next_step_handler(message, add_mp)
+
+######################### закончил тут
+def view_mp(message):
+    if message.text == '<< Назад':
+        bot.send_message(message.from_user.id, 'Вы вернулись в меню управления мероприятиями.', reply_markup=default_mp_action())
+    else:
+        bot.send_message(message.from_user.id, 'Окей, ты выбрал вот это: \n{}'.format(message.text), reply_markup=default_mp_action())
+    bot.register_next_step_handler(message, menu_mp)
+
+def add_mp(message):
+    global mp_name
+    mp_name = message.text
+    bot.send_message(message.from_user.id, "Укажите дату проведения\n(пример 29.01.2021): ")
+    bot.register_next_step_handler(message, add_mp_date)
+
+def add_mp_date(message):
+    global mp_date
+    mp_date = message.text
+    bot.send_message(message.from_user.id, "Укажите время проведения\n(например - 15:00): ")
+    bot.register_next_step_handler(message, add_mp_time)
+
+def add_mp_time(message):
+    global mp_time
+    mp_time = message.text
+    bot.send_message(message.from_user.id, "Укажите погодные условия: ")
+    bot.register_next_step_handler(message, add_mp_weather)
+
+def add_mp_weather(message):
+    global mp_weather
+    mp_weather = message.text
+    bot.send_message(message.from_user.id, "Укажите температуру воздуха: ")
+    bot.register_next_step_handler(message, add_mp_temp)
+
+def add_mp_temp(message):
+    global mp_temp
+    mp_temp = message.text
+    bot.send_message(message.from_user.id, "Укажите количество участников: ")
+    bot.register_next_step_handler(message, add_mp_member)
+
+def add_mp_member(message):
+    global mp_member
+    mp_member = message.text
+    insert_sql_mp(message)
+
+def insert_sql_mp(message):
+    create_mp = "Мероприятие *'{}'*\nуспешно создано.\n*Дата проведения:* {}\n*Время проведения:* {}\n*Погода:* {}\n*Температура:* {}\n*Макс. кол-во участников:* {}\n".format(mp_name, mp_date, mp_time, mp_weather, mp_temp, mp_member)
+    with sqlite3.connect("static/database/main.sqlite") as conn:
+        cursor = conn.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS MP (Id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, MpName TEXT NOT NULL, MpDate TEXT NOT NULL, MpTime TEXT NOT NULL, MpWeather TEXT NOT NULL, MpTemp INTEGER NOT NULL, MpMember INTEGER NOT NULL)")
+        cursor.execute("INSERT INTO MP (MpName, MpDate, MpTime, MpWeather, MpTemp, MpMember) values ('{}', '{}', '{}', '{}', '{}', '{}')".format(mp_name, mp_date, mp_time, mp_weather, mp_temp, mp_member))
+        conn.commit()
+    bot.send_message(message.from_user.id, create_mp, parse_mode="Markdown", reply_markup=default_mp_action())
+    bot.register_next_step_handler(message, menu_mp)
 
 
 def get_car_user(message):
@@ -320,10 +412,9 @@ def default_menu_user():
 
 def default_menu_admin_action():
     carbut = telebot.types.ReplyKeyboardMarkup(True, True)
-    carbut.row('Cоздать гонку')
-    carbut.row('Отсосать')
-    carbut.row('Пойти нахуй')
-    carbut.row('Выйди отсюда розбийнык')
+    carbut.row('Управление мероприятиями')
+    carbut.row('Cписок запросов на регистрацию')
+    carbut.row('<< Назад')
     return carbut
 
 def default_start_button():
@@ -331,7 +422,41 @@ def default_start_button():
     carbut.row('/start')
     return carbut
 
+def default_mp_action():
+    carbut = telebot.types.ReplyKeyboardMarkup(True, True)
+    carbut.row('Список мероприятий')
+    carbut.row('Добавить мероприятие')
+    carbut.row('<< Назад')
+    return carbut
 
+
+def testov(message):
+    result = get_usr()
+    for num in result:
+        bot.send_message(message.chat.id, text="[Запрос регистрации]\nTGUserId: {}\nИмя, Фамилия: {} {}\nТелефон: {}\nДата регистрации: {}\n\n".format(num[3], num[1], num[2], num[4], num[5]), reply_markup=default_menu_admin_action())
+    bot.register_next_step_handler(message, admin_panel)
+
+
+def get_usr():
+    with sqlite3.connect("static/database/main.sqlite") as conn:
+        cursor = conn.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS Users (Id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, FirstName TEXT NOT NULL, LastName TEXT NOT NULL, TGUserId INTEGER NOT NULL, MobilePhone NUMERIC NOT NULL, DateCreated TIMESTAMP DEFAULT CURRENT_TIMESTAMP, Status INTEGER NOT NULL DEFAULT 2)")
+        cursor.execute("SELECT * FROM Users WHERE Status=2")
+        result = cursor.fetchall()
+        conn.commit()
+    return result
+
+
+
+# @bot.callback_query_handler(func=lambda call: True)
+# def query_handler(call):
+#     bot.answer_callback_query(callback_query_id=call.id, text='Права пользователя были изменены.')
+#     answer = ''
+#     if call.data == '10':
+#         answer = 'Регистрация пользователя была одобрена.'
+#     elif call.data == '11':
+#         answer = 'Регистрация пользователя была отклонена.'
+#     bot.send_message(call.message.chat.id, answer)
 
 
 bot.polling(none_stop=True, timeout=500)
